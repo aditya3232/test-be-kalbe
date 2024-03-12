562f77fda14aa6568a0597c17d63d03e41b05cb5
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2024-03-12 13:48:21 +0700

    add repository department

---
+++ internal/repository/repository.go
@@ -0,0 +1,79 @@
+package repository
+
+import (
+	"test-be-kalbe/internal/domain/entity"
+	"test-be-kalbe/internal/domain/model"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type departmentRepository struct {
+	db  *gorm.DB
+	Log *logrus.Logger
+}
+
+type DepartmentRepository interface {
+	Create(db *gorm.DB, entity *entity.Department) error
+	Update(db *gorm.DB, entity *entity.Department) error
+	Delete(db *gorm.DB, entity *entity.Department) error
+	CountById(db *gorm.DB, id int) (int64, error)
+	FindById(db *gorm.DB, entity *entity.Department, id int) error
+	Search(db *gorm.DB, request *model.DepartmentSearchRequest) ([]entity.Department, int64, error)
+	Filter(request *model.DepartmentSearchRequest) func(tx *gorm.DB) *gorm.DB
+}
+
+func NewDepartmentRepository(db *gorm.DB, log *logrus.Logger) DepartmentRepository {
+	return &departmentRepository{
+		db:  db,
+		Log: log,
+	}
+}
+
+func (r *departmentRepository) Create(db *gorm.DB, entity *entity.Department) error {
+	return db.Create(entity).Error
+}
+
+func (r *departmentRepository) Update(db *gorm.DB, entity *entity.Department) error {
+	return db.Save(entity).Error
+}
+
+func (r *departmentRepository) Delete(db *gorm.DB, entity *entity.Department) error {
+	return db.Delete(entity).Error
+}
+
+func (r *departmentRepository) CountById(db *gorm.DB, id int) (int64, error) {
+	var total int64
+	err := db.Model(&entity.Department{}).Where("id = ?", id).Count(&total).Error
+	return total, err
+}
+
+func (r *departmentRepository) FindById(db *gorm.DB, entity *entity.Department, id int) error {
+	return db.Where("id = ?", id).Take(entity).Error
+}
+
+func (r *departmentRepository) Search(db *gorm.DB, request *model.DepartmentSearchRequest) ([]entity.Department, int64, error) {
+	var departements []entity.Department
+	if err := db.Scopes(r.Filter(request)).Offset((request.Page - 1) * request.Size).Limit(request.Size).Find(&departements).Error; err != nil {
+		return nil, 0, err
+	}
+
+	var total int64 = 0
+	if err := db.Model(&entity.Department{}).Scopes(r.Filter(request)).Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	return departements, total, nil
+}
+
+func (r *departmentRepository) Filter(request *model.DepartmentSearchRequest) func(tx *gorm.DB) *gorm.DB {
+	return func(tx *gorm.DB) *gorm.DB {
+
+		if department_name := request.DepartmentName; department_name != "" {
+			department_name = "%" + department_name + "%"
+			tx = tx.Where("name LIKE ?", department_name)
+		}
+
+		return tx
+	}
+}
