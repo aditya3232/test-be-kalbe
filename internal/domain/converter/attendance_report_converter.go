2a0fb77c036fd63d169ba265ffd6edbf87e6fd6f
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2024-03-14 00:42:47 +0700

    attendance report done

--- internal/domain/converter/attendance_report_converter.go
+++ internal/domain/converter/attendance_report_converter.go
@@ -2,17 +2,22 @@ package converter
 
 import (
 	"test-be-kalbe/internal/domain/model"
+	"time"
 )
 
 func AttendanceReportToResponse(attendanceReport model.AttendanceReportResponse) *model.AttendanceReportResponse {
+	parsedDate, _ := time.Parse(time.RFC3339, attendanceReport.Date)
+	parsedAbsentIn, _ := time.Parse(time.RFC3339, attendanceReport.AbsentIn)
+	parsedAbsentOut, _ := time.Parse(time.RFC3339, attendanceReport.AbsentOut)
+
 	return &model.AttendanceReportResponse{
-		Date:           attendanceReport.Date,
+		Date:           parsedDate.Format("2006-01-02 15:04:05"),
 		EmployeeCode:   attendanceReport.EmployeeCode,
 		EmployeeName:   attendanceReport.EmployeeName,
 		DepartmentName: attendanceReport.DepartmentName,
 		PositionName:   attendanceReport.PositionName,
 		LocationName:   attendanceReport.LocationName,
-		AbsentIn:       attendanceReport.AbsentIn,
-		AbsentOut:      attendanceReport.AbsentOut,
+		AbsentIn:       parsedAbsentIn.Format("2006-01-02 15:04:05"),
+		AbsentOut:      parsedAbsentOut.Format("2006-01-02 15:04:05"),
 	}
 }
--- internal/repository/attendance_report_repository.go
+++ internal/repository/attendance_report_repository.go
@@ -27,18 +27,23 @@ func NewAttendanceReportRepository(db *gorm.DB, log *logrus.Logger) *attendanceR
 func (r *attendanceReportRepository) Search(db *gorm.DB, request *model.AttendanceReportSearchRequest) ([]model.AttendanceReportResponse, int64, error) {
 	var attendances []model.AttendanceReportResponse
 
-	// join data from attendance, employee, department, location and position
-	db = db.Table("attendances").
-		Select("attendances.created_at, employees.code as employee_code, employees.name as employee_name, departments.name as department_name, positions.name as position_name, locations.name as location_name, attendances.absent_in, attendances.absent_out").
-		Joins("JOIN employees ON employees.id = attendances.employee_id").
-		Joins("JOIN departments ON departments.id = employees.department_id").
-		Joins("JOIN locations ON locations.id = attendances.location_id").
-		Joins("JOIN positions ON positions.id = employees.position_id")
+	db = db.Table("attendance").
+		Select("attendance.created_at as date, employee.employee_code as employee_code, employee.employee_name as employee_name, department.department_name as department_name, position.position_name as position_name, location.location_name as location_name, attendance.absent_in, attendance.absent_out").
+		Joins("JOIN employee ON employee.employee_id = attendance.employee_id").
+		Joins("JOIN department ON department.department_id = employee.department_id").
+		Joins("JOIN location ON location.location_id = attendance.location_id").
+		Joins("JOIN position ON position.position_id = employee.position_id")
 
 	if err := db.Scopes(r.Filter(request)).Offset((request.Page - 1) * request.Size).Limit(request.Size).Find(&attendances).Error; err != nil {
 		return nil, 0, err
 	}
-	return attendances, 0, nil
+
+	var total int64
+	if err := db.Scopes(r.Filter(request)).Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	return attendances, total, nil
 }
 
 func (r *attendanceReportRepository) Filter(request *model.AttendanceReportSearchRequest) func(tx *gorm.DB) *gorm.DB {
@@ -69,13 +74,13 @@ func (r *attendanceReportRepository) Filter(request *model.AttendanceReportSearc
 			}
 
 			// Apply time interval condition to the query
-			tx = tx.Where("attendances.created_at >= ? AND attendances.created_at <= ?", startTime, endTime)
+			tx = tx.Where("attendance.created_at >= ? AND attendance.created_at <= ?", startTime, endTime)
 		} else {
 			// If no TimeInterval specified, fetch all data
-			tx = tx.Where("attendances.created_at IS NOT NULL")
+			tx = tx.Where("attendance.created_at IS NOT NULL")
 		}
 
-		tx = tx.Where("attendances.deleted_at IS NULL")
+		tx = tx.Where("attendance.deleted_at IS NULL")
 
 		return tx
 	}
